d702200d2c60f08493c2c8a84f1341c8e2244757
unknown <debmalyapaine@gmail.com>
2024-06-21 12:07:20 +0530

    add: tcp listener

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello Redis!")
+	// Listen on TCP port 6379
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer listener.Close()
+
+	fmt.Println("Redis server started on port 6379")
+
+	// Accept new connections from the Listener perpetually
+	for {
+		// This will block till a new connection is received
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	conn.Write([]byte("+OK"))
 }
