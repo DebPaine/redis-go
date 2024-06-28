b3f3e0f036e9096591dc2421aba58771e275ffe0
DebPaine <debmalyapaine@gmail.com>
2024-06-28 23:44:29 +0530

    add: writeResp and marshal logic

--- resp/write.go
+++ resp/write.go
@@ -1,9 +1,48 @@
 package resp
 
+import (
+	"bufio"
+	"fmt"
+)
+
 /*
 1. In this write.go file, we serialize the response accroding to RESP and write back to the client
 */
+func WriteResp(w *bufio.Writer, v Value) error {
+	bytes, err := v.Marshal()
+	if err != nil {
+		return err
+	}
 
-func (v *Value) Marshal() []byte {
-	return []byte("OK")
+	_, err = w.Write(bytes)
+	if err != nil {
+		return err
+	}
+	return w.Flush()
+}
+
+func (v *Value) Marshal() ([]byte, error) {
+	switch v.typ {
+	case "string":
+		return v.marshalString()
+	case "bulk":
+		return v.marshalBulkString()
+	case "array":
+	case "int":
+	case "null":
+	case "error":
+	default:
+		return []byte{}, fmt.Errorf("Unexpected type: %s", v.typ)
+	}
+	return []byte{}, fmt.Errorf("Unexpected type: %s", v.typ)
+}
+
+func (v *Value) marshalString() ([]byte, error) {
+	// eg:  +OK\r\n
+	return []byte(STRING + v.str + "\r\n"), nil
+}
+
+func (v *Value) marshalBulkString() ([]byte, error) {
+	// eg: $5\r\nhello\r\n
+	return []byte(BULK + string(len(v.bulk)) + "\r\n" + v.bulk + "\r\n"), nil
 }
